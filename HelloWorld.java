f4f649038f861b9b70126cf3758ec61e6bef5bb7
sravs2022 <98410974+sravs2022@users.noreply.github.com>
2022-07-11 23:51:48 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
+    System.out.println("Hello, Devops guys,how are you");
+    System.out.println("Hello, Devops guys,how are you");
+    System.out.println("Hello, Devops guys,how are you");
+    System.out.println("Hello, Devops guys,how are you");
   }
 }
 
